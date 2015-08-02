6c680f99bc7889a2560afb2ddd2ad481f6847f0b
Kevin Haller <kevin.haller@outofbits.com>
2015-08-02 22:44:26 +0200

    Transfer to less-stylesheets.
    The base css stylesheet is now generated by the less/ccshuffle.less file.
    Minor changes for the template files. The svg element is now stored in
    an own file (included by the corresponding template), because of readability.

---
+++ static/less/lsscompile.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# You have to install the less compiler to run this command.
+# Linux: http://stackoverflow.com/questions/7245826/less-compiler-for-linux
+
+lessc 'ccshuffle.less' > '../css/ccshuffle.css'
