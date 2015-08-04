d5a36b72ca0b498e3569ecfd0299d67c7b38f44d
Kevin Haller <kevin.haller@outofbits.com>
2015-08-04 05:53:51 +0200

    Design change of the about (information) page - transformation to less.
     The special css of the about page is now generated by a less file like
     the base css file.

--- static/less/lsscompile.sh
+++ static/less/lsscompile.sh
@@ -4,3 +4,4 @@
 # Linux: http://stackoverflow.com/questions/7245826/less-compiler-for-linux
 
 lessc 'ccshuffle.less' > '../css/ccshuffle.css'
+lessc 'ccshuffle-about.less' > '../css/ccshuffle-about.css'
