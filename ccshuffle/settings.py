0934e33cc61928755cda4990185b9f7e22d2f513
Kevin Haller <kevin.haller@outofbits.com>
2015-08-31 02:48:15 +0200

    The settings are now ready for production. Configuration is easy modifiable.
     The settings file loads the configuration from the CONF_FILE. The CON_FILE
     is per default the ccshuffle.conf file in the base directory. To change this
     the enviroment variable 'CCSHUFFLE_CONF' can be set.

--- ccshuffle/settings.py
+++ ccshuffle/settings.py
@@ -12,20 +12,48 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys, traceback
+import json
+from django.utils.translation import ugettext_lazy as _
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Configuration file for the deployment. The default location is the file with the name 'ccshuffle.conf' in the
+# base directory. If the environment variable 'CCSHUFFLE_CONF' is set, the value of the variable as path will be used.
 
-# Quick-start development settings - unsuitable for production
+CONF_FILE = os.environ['CCSHUFFLE_CONF'] if 'CCSHUFFLE_CONF' in os.environ else '%s/ccshuffle.conf' % BASE_DIR
+
+# Development settings
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&*lu()8=bw%embg-@&n9h=q5w&s=&1=9fn@1xgejo3m1-d+x76'
+try:
+    with open(CONF_FILE, 'r', encoding='utf-8') as fp:
+        conf = json.load(fp)
+        # The secret key must be a large random value and it must be kept secret.
+        SECRET_KEY = conf['SECRET_KEY']
+        # True for development, False for production (the ALLOWED_HOSTs must be set then).
+        DEBUG = conf['DEBUG'] if 'DEBUG' in conf else False
+        if not DEBUG:
+            # The hosts, which are allowed to use the service.
+            ALLOWED_HOSTS = conf['ALLOWED_HOSTS']
+            # The csrf cookie enforces to be transported over https.
+            # https://docs.djangoproject.com/en/1.8/ref/settings/#std:setting-CSRF_COOKIE_SECURE
+            CSRF_COOKIE_SECURE = conf['CSRF_COOKIE_SECURE'] if 'CSRF_COOKIE_SECURE' in conf else True
+            # The session cookie enforces to be transported over https.
+            # https://docs.djangoproject.com/en/1.8/ref/settings/#std:setting-SESSION_COOKIE_SECURE
+            SESSION_COOKIE_SECURE = conf['SESSION_COOKIE_SECURE'] if 'SESSION_COOKIE_SECURE' in conf else True
+        # Database
+        # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+        DATABASES = conf['DATABASES']
+        # Logging settings
+        # https://docs.djangoproject.com/en/1.8/topics/logging/
+        LOGGING = conf['LOGGING']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+except Exception as e:
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    traceback.print_exception(exc_type, exc_value, exc_traceback, limit=10, file=sys.stdout)
+    print('The configuration file (%s) can\'t be loaded.' % CONF_FILE, file=sys.stderr)
+    exit(1)
 
 
 # Application definition
@@ -73,17 +101,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ccshuffle.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
@@ -97,46 +114,20 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = (
+    'locale',
+)
+
+# Languages, which are supported by the service.
+LANGUAGES = (
+    ('de', _('German')),
+    ('en', _('English')),
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.getcwd() + "/static",)
-
-
-# Locale Path
-# https://docs.djangoproject.com/en/1.8/topics/i18n/translation/
-LOCALE_PATHS = (
-    'locale',
-)
-
-from django.utils.translation import ugettext_lazy as _
-
-LANGUAGES = (
-    ('de', _('German')),
-    ('en', _('English')),
-)
-
-# Logging settings
-# https://docs.djangoproject.com/en/1.8/topics/logging/
-
-'''LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'shuffle.log',
-        },
-    },
-    'loggers': {
-        'shuffle': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}'''
+STATICFILES_DIRS = (BASE_DIR + "/static",)
