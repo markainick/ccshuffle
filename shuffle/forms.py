28425f4d5f7a3dc7dfb9024504f76fcc93412c35
Kevin Haller <kevin.haller@outofbits.com>
2015-09-22 17:37:22 +0200

    Makes pyflakes test cases succeed - Removes unused imports.

--- shuffle/forms.py
+++ shuffle/forms.py
@@ -13,7 +13,6 @@
 
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.forms import (AuthenticationForm, UserCreationForm)
 
 import re
